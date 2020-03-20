eab1d4f975f88d0ea78f01d0325fffe6728bda06
raglandcodes <raglandasir@gmail.com>
2020-03-20 22:06:07 +0530

    set up for custom sign in button

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,31 +1,51 @@
 import { dataFetch } from './dataFetch';
 
+const singInBtn = document.querySelector('#GSingInBtn');
 let userState = {
     signedIn: false,
 };
 
-async function onSignIn(userProfile) {
-    console.dir(userProfile);
-    let idToken = userProfile.getAuthResponse().id_token;
+var googleUser = {};
+function startApp() {
+    gapi.load('auth2', function() {
+        var auth2 = gapi.auth2.init({
+            client_id:
+                '906977270322-n4u9v0sua99hiqbjtmsjg7tfji2qihqo.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+        });
+        auth2.attachClickHandler(
+            singInBtn,
+            {},
+            googleUser => {
+                verifyIdToken(googleUser.uc.id_token);
+            },
+            error => {
+                alert(JSON.stringify(error, undefined, 2));
+            }
+        );
+    });
+}
 
+async function verifyIdToken(idToken) {
     let signInRespnse = await dataFetch(
         'POST',
         'verifyIdToken',
         { token: idToken },
         null
     );
-
     if (signInRespnse.status === 'OK') {
         userState['signedIn'] = true;
+        console.log('Verified');
     } else {
         console.log(
             `${JSON.stringify(signInRespnse, null, 2)} <= signInRespnse`
         );
     }
-    console.log(`${JSON.stringify(userState, null, 2)} <= userState`);
 }
 
-//TODO give a different name
-window.onSignIn = onSignIn;
-
-export { userState };
+//TODO do this earlier.
+singInBtn.addEventListener('click', () => {
+    startApp();
+});
+// window.WINDOWSTARTAPP = startApp;
+export { userState, startApp };
