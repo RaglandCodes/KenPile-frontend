ed8ce456b4ef11e3da42aeb10e9ce7ef37dd3ea9
raglandcodes <raglandasir@gmail.com>
2020-04-26 17:56:56 +0530

    Try Netlify CI

--- src/scripts/dataFetch.js
+++ src/scripts/dataFetch.js
@@ -1,7 +1,7 @@
 // This is meant to talk with the KenPile back end server
 
-const baseURL = 'http://localhost:5000';
-//const baseURL = 'https://kenpile.herokuapp.com';
+//const baseURL = 'http://localhost:5000';
+const baseURL = 'https://kenpile.herokuapp.com';
 
 async function dataFetch(method, route, headers, body) {
     let queryURL = `${baseURL}/${route}`;
